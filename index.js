7595a152f1b3a04368331c82f15d8b9cbd74befc
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-05-06 22:48:44 +0530

    route create to publish blog

--- index.js
+++ index.js
@@ -7,12 +7,29 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("node_modules/bootstrap/dist"));
 app.use(express.static("public"));
 
+let blogs = [];
+
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { blogs });
 });
 
 // CRUD routes
-app.post("/create", (req, res) => {});
+
+app.post("/create", (req, res) => {
+  const { title, description, content } = req.body;
+  const newBlog = {
+    id: blogs.length + 1,
+    title,
+    description,
+    content,
+    createdDate: new Date().toDateString(),
+  };
+  console.log(newBlog);
+
+  blogs.push(newBlog);
+
+  res.redirect("/");
+});
 app.get("/view", (req, res) => {});
 app.put("/edit", (req, res) => {});
 app.delete("/delete", (req, res) => {});
