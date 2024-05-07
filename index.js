f67aaa4b64d77e247539d0c8a81c8811d6d5d734
immkavin-ranks <immkavin.ofcl@gmail.com>
2024-05-07 20:18:26 +0530

    Add routes edit and save

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ app.post("/create", (req, res) => {
     content,
     createdDate: new Date().toDateString(),
   };
-  console.log(newBlog);
 
   blogs.push(newBlog);
 
@@ -37,7 +36,30 @@ app.get("/view/:id", (req, res) => {
 
   res.render("blog.ejs", { blog, isViewRoute: true });
 });
-app.put("/edit", (req, res) => {});
+
+
+app.get("/edit/:id", (req, res) => {
+  const blogId = req.params.id;
+  const blog = blogs[blogId - 1];
+  console.log(blog);
+  res.render("editBlog.ejs", { blog });
+});
+
+app.put("/save/:id", (req, res) => {
+  const blogId = req.params.id;
+  const { title, description, content } = req.body;
+
+  // Find the index of the blog in the array
+  const index = blogId - 1;
+
+  // Update the blog with the new values
+  blogs[index].title = title;
+  blogs[index].description = description;
+  blogs[index].content = content;
+
+  res.redirect("/");
+});
+
 app.delete("/delete", (req, res) => {});
 
 app.listen(PORT, () => {
